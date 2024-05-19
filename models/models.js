dc22bfe99ac7709de6e636c99facb28ab4078f85
jayantsingh-22 <jayantsingh640@gmail.com>
2024-05-19 10:57:06 +0530

    refactor:added partial Indexing of the email field in userSchema

--- models/models.js
+++ models/models.js
@@ -1,23 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const customPropertySchema = new mongoose.Schema({
-    title: String,
-    fallbackValue: String
+  title: String,
+  fallbackValue: String,
 });
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: { type: String, unique: true },
-    customProperties: { type: Map, of: String }
+  name: String,
+  email: { type: String, unique: true, sparse: true },
+  customProperties: { type: Map, of: String },
 });
+
+userSchema.index(
+  { email: 1 },
+  {
+    unique: true,
+    partialFilterExpression: { email: { $exists: true, $ne: null } },
+  }
+);
 
 const listSchema = new mongoose.Schema({
-    title: String,
-    customProperties: [customPropertySchema],
-    users: { type: [userSchema], default: [] },
-    unsubscribedEmails: { type: [String], default: [] }
+  title: String,
+  customProperties: [customPropertySchema],
+  users: { type: [userSchema], default: [] },
+  unsubscribedEmails: { type: [String], default: [] },
 });
 
-const List = mongoose.model('List', listSchema);
+const List = mongoose.model("List", listSchema);
 
 module.exports = { List };
